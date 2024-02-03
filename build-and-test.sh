0335938274bab824cf2346b0cbf9682325d3976c
Ahmed Daker <ahmeddakerwork@gmail.com>
2024-02-03 12:45:39 +0000

    udpating build-and-test.sh

--- build-and-test.sh
+++ build-and-test.sh
@@ -3,12 +3,12 @@
 VERSION=$(cat VERSION)
 PYVERSION=$(cat python/PYVERSION)
 
-docker build . -t developmentseed/geolambda:${VERSION}
-docker run --rm -v $PWD:/home/geolambda -it developmentseed/geolambda:${VERSION} package.sh
+docker build . -t ahmeddaker/geolambda:${VERSION}
+docker run --rm -v $PWD:/home/geolambda -it ahmeddaker/geolambda:${VERSION} package.sh
 
 cd python
-docker build . --build-arg VERSION=${VERSION} -t developmentseed/geolambda:${VERSION}-python
-docker run -v ${PWD}:/home/geolambda -t developmentseed/geolambda:${VERSION}-python package-python.sh
+docker build . --build-arg VERSION=${VERSION} -t ahmeddaker/geolambda:${VERSION}-python
+docker run -v ${PWD}:/home/geolambda -t ahmeddaker/geolambda:${VERSION}-python package-python.sh
 
 docker run -e GDAL_DATA=/opt/share/gdal -e PROJ_LIB=/opt/share/proj \
     --rm -v ${PWD}/lambda:/var/task lambci/lambda:python3.10 lambda_function.lambda_handler '{}'
